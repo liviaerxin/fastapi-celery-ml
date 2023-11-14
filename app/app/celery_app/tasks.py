014db0420b715fe7b68cb79d30fd5573272b1cfc
liviaerxin <1yue8haogaoqi@gmail.com>
2023-11-15 00:37:35 +0800

    update tasks.py, celery_workflow.py and docker-compose.yml

--- app/app/celery_app/tasks.py
+++ app/app/celery_app/tasks.py
@@ -1,9 +1,10 @@
-from celery import Celery, Task, chord, group
-from celery.result import AsyncResult, allow_join_result
-from typing import List
+from celery import Celery, Task, chord, group, subtask, chain
+from celery.result import AsyncResult, allow_join_result, GroupResult
+from typing import List, Any
 import os
 import sys
 import time
+import math
 
 
 class CeleryConfig:
@@ -28,12 +29,39 @@ def echo(msg: str) -> str:
     return f"echo() - message[{msg}]"
 
 
+@app.task()
+def log(msg: Any) -> Any:
+    time.sleep(1)
+    return f"log() - message[{msg}]"
+
+
 @app.task(acks_late=True)
 def wait(secs: float) -> str:
     print(f"wait() - Start, secs[{secs}]s")
     time.sleep(secs)
     print(f"wait() - Done, secs[{secs}]s")
     return f"wait() - Done, secs[{secs}]s"
+
+
+@app.task(bind=True, acks_late=True)
+def progress(self, secs: float) -> List[int]:
+    print(f"progress() - Start, secs[{secs}]s")
+    secs = math.ceil(secs)
+    total = 100
+    n = 0
+    result = []
+    while n <= secs:
+        self.update_state(
+            state="PROGRESS",
+            meta={
+                "current": (n / secs) * total,
+                "total": total,
+            },
+        )
+        result.append(n)
+        n = n + 1
+        time.sleep(1)
+    return result
 
 
 @app.task(bind=True)
@@ -85,4 +113,22 @@ def mapreduce(data: List[str]):
         result = task.get()
     # task.get(disable_sync_subtasks=False)
     print(f"mapreduce() - result[{result}]")
+    return result
+
+
+@app.task()
+def dmap(results, t4, t5, t6):
+    # Map a task over an result and return as a group
+
+    # subtask is a task with parameters passed, but not yet started.
+    t4 = subtask(t4)
+    t5 = subtask(t5)
+    t6 = subtask(t6)
+
+    print(results[0])
+    # iterating over result and cloning task
+    header = [t4.clone((results[0],))]
+    header.extend([t5.clone((arg,)) for arg in results[1]])
+    task = chord(header=header, body=t6)
+    result = task.apply_async()
     return result
--- app/examples/celery_workflow.py
+++ app/examples/celery_workflow.py
@@ -3,8 +3,13 @@ from celery.canvas import Signature
 from celery.result import AsyncResult, allow_join_result, GroupResult, ResultSet
 
 import time
-from app.celery_task_app import tasks
+from app.celery_app import tasks
 import uuid
+import json
+
+
+def print_task(task: Signature):
+    print(f"{json.dumps(task, indent=2)}")
 
 
 def store(node):
@@ -27,18 +32,20 @@ def restore(id_chain):
 
 
 def run_chord():
-    task = chord([tasks.add.s(2, 2), tasks.add.s(3, 3)])(tasks.test_celery.s())
-    print(f"{task.get()}")
+    result: AsyncResult = chord([tasks.add.s(2, 2), tasks.add.s(3, 3)])(
+        tasks.test_celery.s()
+    )
+    print(f"{result.get()}")
 
 
 def run_group():
-    task: GroupResult = group(
+    result: GroupResult = group(
         [tasks.add.s(2, 2), tasks.add.s(3, 3), tasks.add.s(4, 4)]
     ).apply_async()
-    print(f"#{task.id} {task.get()}")
+    print(f"#{result.id} {result.get()}")
 
-    task.save()
-    saved_result = GroupResult.restore(task.id)
+    result.save()
+    saved_result = GroupResult.restore(result.id)
 
     print(f"#{saved_result.id} {saved_result.get()}")
 
@@ -64,6 +71,8 @@ def get_chain_graph():
             tasks.add.s(2, 2), 
             tasks.add.s(3)
         )
+    print_task(task)
+
     # fmt: on
     result: AsyncResult = (
         task.freeze()
@@ -81,6 +90,8 @@ def get_group_graph():
             tasks.add.s(2, 2), 
             tasks.add.s(3, 3)
         ])
+    print_task(task)
+
     # fmt: on
     result: GroupResult = (
         task.freeze()
@@ -93,6 +104,7 @@ def get_group_graph():
 
 def get_chord_graph():
     task = chord(header=[tasks.add.s(2, 2), tasks.add.s(3, 3)], body=tasks.add.si(4, 2))
+    print_task(task)
     # task = chord(header=[tasks.add.s(2, 2), tasks.add.s(3, 3)], body=group([tasks.add.si(4, 2), tasks.add.si(4, 3)]))
     result: AsyncResult = (
         task.freeze()
@@ -110,6 +122,8 @@ def run_chord_graph(dryrun=False, wait=True):
         header=[tasks.add.s(2, 2), tasks.add.s(3, 3)],
         body=group([tasks.add.si(4, 2), tasks.add.si(4, 3)]),
     )
+    print_task(task)
+
     result: AsyncResult = (
         task.freeze() if dryrun else task.apply_async()
     )  # `freeze()` Dry run to give underlying running steps
@@ -136,30 +150,29 @@ def run_signature(dryrun=False):
         print(f"signature: [{result.get()}]")
 
 
-def run_progress():
+def show_progress():
     t1: Signature = tasks.progress.s(10).set(task_id=str(uuid.uuid4()))
     t2: Signature = tasks.progress.s(5).set(task_id=str(uuid.uuid4()))
 
     # t3: Signature = tasks.add.si(4, 4).set(task_id=str(uuid.uuid4()))
     # t4: Signature = tasks.add.si(5, 5).set(task_id=str(uuid.uuid4()))
-    t3: Signature = tasks.test_celery.s().set(task_id=str(uuid.uuid4()))
-    t4: Signature = tasks.test_celery.s().set(task_id=str(uuid.uuid4()))
-    
+    t3: Signature = tasks.log.s().set(task_id=str(uuid.uuid4()))
+    t4: Signature = tasks.log.s().set(task_id=str(uuid.uuid4()))
 
     task = chord(header=[t1, t2], body=group([t3, t4]))
     result: AsyncResult = task.apply_async()
-    
+
     print(f"task: {task}")
     print(f"graph: {result.as_tuple()}")
     print(f"result: #[{result}]")
-    
+
     res1 = AsyncResult(id=t1.id)
     res2 = AsyncResult(id=t2.id)
     res3 = AsyncResult(id=t3.id)
     res4 = AsyncResult(id=t4.id)
 
     ress = ResultSet(results=[res1, res2, res3, res4])
-    
+
     while not result.ready():
         print(f"result #1, result[{res1.result}] status[{res1.status}]")
         print(f"result #2, result[{res2.result}] status[{res2.status}]")
@@ -175,12 +188,38 @@ def run_progress():
     print(f"result set[#1, #2, #3, #4] {ress.completed_count()} {ress.ready()}")
     print(f"result: {result.get()}")
 
-        self.update_state(
-            state="PROGRESS",
-            meta={"current": current, "total": total, "message": message},
-        )
+
+def run_():
+    # t1 + t2 > t3 > t4 + (t51, t52, t53, t54, t55, t56) > t6
+    t1 = tasks.add.s(4, 5).set(task_id=str(uuid.uuid4()))
+    t2 = tasks.progress.s(10).set(task_id=str(uuid.uuid4()))
+    # print_task(t1)
+    # print_task(t2)
+
+    t4 = tasks.progress.s()
+    t5 = tasks.wait.s()
+
+    t6 = tasks.log.s()
+
+    t3 = tasks.dmap.s(t4, t5, t6)
+
+    t = chain(group([t1, t2]), t3)
+    print_task(t)
+
+    result: AsyncResult = t.apply_async()
+
+    print(result.get())
+
+    chord_task_id = result.get()[0][0]
+
+    print(chord_task_id)
+
+    print(AsyncResult(id=chord_task_id).get())
+
+
 if __name__ == "__main__":
     # run_chord_graph(dryrun=True)
     # run_chord_graph(dryrun=False, wait=True)
     # run_signature(dryrun=False)
-    run_progress()
+    # show_progress()
+    run_()
